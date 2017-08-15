bad4ccbb0270381581ff13a8d1b0808d4bf6cfac
Greg Tatum <tatum.creative@gmail.com>
2017-08-15 08:54:23 -0500

    Examples moving triangles

--- examples/moving-triangles.rs
+++ examples/moving-triangles.rs
@@ -5,8 +5,8 @@ fn main() {
     let mut rugl = rugl::init();
     let count = 1000;
 
-    let draw = rugl.draw()
-        .vert("
+    let draw_moving_triangles = rugl!(rugl.draw, {
+        vert => "
             #version 150
             in vec2 position;
             in float id;
@@ -28,8 +28,8 @@ fn main() {
                     1.0
                 );
             }
-        ")
-        .frag("
+        ",
+        frag => "
             #version 150
             out vec4 out_color;
             uniform vec3 color;
@@ -38,33 +38,36 @@ fn main() {
             void main() {
                 out_color = vec4(color + color2 + color3, 1.0);
             }
-        ")
-        .uniform("color", Box::new(|_| Box::new([1.0, 0.0, 0.0])))
-        .uniform("color2", Box::new(|_| Box::new([0.0, 1.0, 0.0])))
-        .uniform("color3", Box::new(|_| Box::new([0.0, 0.0, 1.0])))
-        .uniform("time", Box::new(|env| Box::new(env.time as f32)))
-        .uniform("count", {
-            let count_in = count.clone() as f32;
-            Box::new(move |_| Box::new(count_in))
-        })
-        .attribute("position", {
-            &((0..(count * 3)).map(|i| {
-                // Equilateral triangle
-                match i % 3 {
-                    0 => [0.0, 0.5],
-                    1 => [0.36056, -0.5],
-                    2 => [-0.36056, -0.5],
-                    _ => panic!("Math is hard.")
-                }
-            }).collect::<Vec<[f32; 2]>>())
-        })
-        .attribute("id", {
-            &((0..(count * 3)).map(|i| {
-                (i as f32 / 3.0).floor()
-            }).collect::<Vec<f32>>())
-        })
-        .count(count * 3)
-        .finalize();
+        ",
+        uniforms => {
+            color => { |_| Box::new([1.0, 0.0, 0.0]) },
+            color2 => { |_| Box::new([0.0, 1.0, 0.0]) },
+            color3 => { |_| Box::new([0.0, 0.0, 1.0]) },
+            time => { |env| Box::new(env.time as f32) },
+            count => {
+                let count_in = count.clone() as f32;
+                move |_| Box::new(count_in)
+            }
+        },
+        attributes => {
+            position => {
+                &((0..(count * 3)).map(|i| {
+                    // Equilateral triangle
+                    match i % 3 {
+                        0 => [0.0, 0.5],
+                        1 => [0.36056, -0.5],
+                        _ => [-0.36056, -0.5]
+                    }
+                }).collect::<Vec<[f32; 2]>>())
+            },
+            id => {
+                &((0..(count * 3)).map(|i| {
+                    (i as f32 / 3.0).floor()
+                }).collect::<Vec<f32>>())
+            }
+        },
+        count => { count * 3 }
+    });
 
     let clear = rugl!(rugl.clear, {
         color => [0.3, 0.2, 0.3, 1.0],
@@ -73,6 +76,6 @@ fn main() {
 
     rugl.frame(|env| {
         clear();
-        draw(env);
+        draw_moving_triangles(env);
     });
 }
